21cdead18f3b90a18fb178a170b5b50691d84bdb
Emmanuel Jesus Estallo <erstallo@up.edu.ph>
2021-09-12 17:46:57 +0800

    16nm NMOS CS stage - resistive load

--- w_factor.py
+++ w_factor.py
@@ -1,7 +1,7 @@
 gm_des = 500*(10**(-6))
-gm_curr = 566.55*(10**(-9))
+gm_curr = 88.62*(10**(-6))
 
-curr_width = 1*(10**(-6))
+curr_width = 160*(10**(-9))
 
 mult_fact = gm_des/gm_curr 
 new_width = curr_width*mult_fact
