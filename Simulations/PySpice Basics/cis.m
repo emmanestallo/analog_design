5b9e52749b10c0609b2b5b3f6a5ebcef928e9b27
Emmanuel Jesus Estallo <erstallo@up.edu.ph>
2021-10-02 15:53:03 +0800

    simulation and plotting

--- Simulations/PySpice Basics/cis.m
+++ Simulations/PySpice Basics/cis.m
@@ -8,10 +8,20 @@ y_sq1 = y_1.^2;
 y_sq2 = y_2.^2;
 y_sq3 = y_3.^2;
 
-
+figure
 plot(x,y_1,'r')
 hold on 
 plot(x,y_2,'g')
 plot(x,y_3,'b') 
 legend('n=1','n=2','n=3')
 title('Wave Function')
+grid on
+
+figure 
+plot(x,y_sq1,'r')
+hold on 
+plot(x,y_sq2,'g')
+plot(x,y_sq3,'b') 
+legend('n=1','n=2','n=3')
+title('Probability Density')
+grid on 
