7a994e93cdcb11a55756cc02d4a532979ce39903
Emmanuel Jesus Estallo <erstallo@up.edu.ph>
2021-10-03 13:43:24 +0800

    updates
    probability density

---
+++ Simulations/PySpice Basics/cis_3.m
@@ -0,0 +1,26 @@
+x = linspace(0,1,100);
+
+n_1_1 = surf_3d(1,1,x).^2;
+n_1_2 = surf_3d(1,2,x).^2; 
+n_2_1 = surf_3d(2,1,x).^2;
+n_2_2 = surf_3d(2,2,x).^2; 
+
+figure 
+plot(x,n_1_1)
+title('n_x = 1, n_y = 1')
+grid on
+
+figure 
+plot(x,n_1_2)
+title('n_x = 1, n_y = 2')
+grid on
+
+figure 
+plot(x,n_2_1)
+title('n_x = 2, n_y = 1')
+grid on
+
+figure 
+plot(x,n_2_2)
+title('n_x = 2, n_y = 2')
+grid on
